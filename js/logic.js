79b1ea671cd995c2a0e642aaeab881cfb26f329d
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-06-11 18:31:11 -0700

    update

---
+++ js/logic.js
@@ -0,0 +1,51 @@
+// Menu
+
+$(document).ready(function ($) {
+    //open menu
+    $('.cd-menu-trigger').on('click', function (event) {
+        event.preventDefault();
+        $('#cd-main-content').addClass('move-out');
+        $('#main-nav').addClass('is-visible');
+        $('.cd-shadow-layer').addClass('is-visible');
+    });
+    //close menu
+    $('.cd-close-menu').on('click', function (event) {
+        event.preventDefault();
+        $('#cd-main-content').removeClass('move-out');
+        $('#main-nav').removeClass('is-visible');
+        $('.cd-shadow-layer').removeClass('is-visible');
+    });
+
+    //clipped image - blur effect
+    set_clip_property();
+    $(window).on('resize', function () {
+        set_clip_property();
+    });
+
+    function set_clip_property() {
+        var $header_height = $('.cd-header').height(),
+            $window_height = $(window).height(),
+            $header_top = $window_height - $header_height,
+            $window_width = $(window).width();
+        $('.cd-blurred-bg').css('clip', 'rect(' + $header_top + 'px, ' + $window_width + 'px, ' + $window_height + 'px, 0px)');
+    }
+});
+
+// Swiper
+
+var swiper = new Swiper('.swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
+
+// 
+
