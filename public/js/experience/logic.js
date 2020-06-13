21dcbfba4f7e93ea78d125f30951fbd39d31c1c3
Adam Birgenheier <adamcarlbirgenheier@gmail.com>
2020-06-13 16:29:27 -0700

    finished bottom section w/ simple footer. Simple errors and bug fixes still need to be done. Almost complete

--- public/js/experience/logic.js
+++ public/js/experience/logic.js
@@ -77,10 +77,3 @@ $(() => {
 // 
 // 
 // 
-var swiper = new Swiper('.swiper-container', {
-    direction: 'vertical',
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-});
